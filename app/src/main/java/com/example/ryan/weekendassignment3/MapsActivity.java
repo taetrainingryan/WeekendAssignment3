f4c0c2f005f282e2b0db393027073718237461b3
taetrainingryan <33837187+taetrainingryan@users.noreply.github.com>
2017-12-01 14:40:43 +0000

    Use case 1 done, puts all markers onto map

--- app/src/main/java/com/example/ryan/weekendassignment3/MapsActivity.java
+++ app/src/main/java/com/example/ryan/weekendassignment3/MapsActivity.java
@@ -2,7 +2,11 @@ package com.example.ryan.weekendassignment3;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.example.ryan.weekendassignment3.model.ParkingSpot;
+import com.example.ryan.weekendassignment3.services.RequestInterface;
+import com.example.ryan.weekendassignment3.services.ServerConnection;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,37 +14,89 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.List;
+
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private RequestInterface requestInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+        requestInterface = ServerConnection.getServerConnection();
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        getData();
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng sydney = new LatLng(37.774074, -122.422068);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    public void addMarkers(ParkingSpot parkingSpot, GoogleMap googleMap){
+
+        //double lat= Double.parseDouble(parkingSpot.getLat());
+       // double lng= Double.parseDouble(parkingSpot.getLng());
+
+        LatLng location = new LatLng(parkingSpot.getLat(), parkingSpot.getLng());
+        mMap.addMarker(new MarkerOptions().position(location).title(parkingSpot.getId().toString()).snippet(parkingSpot.getLat() + " " + parkingSpot.getLng()));
+
+    }
+
+    public void getData(){
+
+        requestInterface.getParkingSpots()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Observer<List<ParkingSpot>>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(List<ParkingSpot> value) {
+
+                        for (ParkingSpot r : value) {
+                            addMarkers(r, mMap);
+                        }
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                        e.printStackTrace();
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                        LatLng sydney = new LatLng(37.774074, -122.422068);
+                        mMap.moveCamera( CameraUpdateFactory.newLatLngZoom(sydney, 12.0f));
+
+                    }
+                });
     }
 }
