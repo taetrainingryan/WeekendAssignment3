83080b53cb4ba870344cf125f0a80a3452d664e9
taetrainingryan <33837187+taetrainingryan@users.noreply.github.com>
2017-12-03 21:24:56 +0000

    Revert "Revert "Offline reservations page done.""
    This reverts commit a4e77baca4067893870565edb0bafdbbc040f6dc.

--- app/src/main/java/com/example/ryan/weekendassignment3/MainActivity.java
+++ app/src/main/java/com/example/ryan/weekendassignment3/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
     private FragmentManager fragmentManager;
+    BottomNavigationView navigation;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -19,14 +21,22 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
+
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
+                    //mTextMessage.setText(R.string.title_home);
+                    switchToFragment1();
+                    break;
+
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
+                    //mTextMessage.setText(R.string.title_dashboard);
+                    switchToFragment2();
+                    break;
+
+                    default: return false;
             }
-            return false;
+
+            //updateNavigationBarState(item.getItemId());
+            return true;
         }
     };
 
@@ -41,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
 
         fragmentManager = getSupportFragmentManager();
 
-
         mTextMessage = (TextView) findViewById(R.id.message);
         navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
@@ -50,5 +59,22 @@ public class MainActivity extends AppCompatActivity {
                 .add(R.id.fragment_container, new MapsFragment())
                 .commit();
     }
+
+    public void switchToFragment1() {
+        fragmentManager.beginTransaction().replace(R.id.fragment_container, new MapsFragment()).commit();
+    }
+
+    public void switchToFragment2() {
+        fragmentManager.beginTransaction().replace(R.id.fragment_container, new ReservationsFragment()).commit();
+    }
+
+//    private void updateNavigationBarState(int actionId){
+//        Menu menu = navigation.getMenu();
+//
+//        for (int i = 0, size = menu.size(); i < size; i++) {
+//            MenuItem item = menu.getItem(i);
+//            item.setChecked(item.getItemId() == actionId);
+//        }
+//    }
 
 }
