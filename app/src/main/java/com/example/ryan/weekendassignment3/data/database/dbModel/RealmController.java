358bd49f4e785a946d3982060ed075c1aa719965
taetrainingryan <33837187+taetrainingryan@users.noreply.github.com>
2017-12-03 21:29:24 +0000

    Revert "Revert "Revert "Revert "Offline reservations page done.""""
    This reverts commit 41893c9907afcd88880e87935fa1d6377fac437c.

---
+++ app/src/main/java/com/example/ryan/weekendassignment3/data/database/dbModel/RealmController.java
@@ -0,0 +1,83 @@
+package com.example.ryan.weekendassignment3.data.database.dbModel;
+
+/**
+ * Created by Ryan on 03/12/2017.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
+
+/**
+ * Created by Ryan on 25/11/2017.
+ */
+
+public class RealmController {
+
+    private static RealmController instance = null;
+
+    public static RealmController getInstance(){
+
+        synchronized (RealmController.class){
+
+            if(instance == null){
+
+                synchronized (RealmController.class){
+
+                    instance = new RealmController(Realm.getDefaultInstance());
+
+                }
+            }
+        }
+
+        return instance;
+    }
+
+    private Realm realm;
+
+    public RealmController(Realm realm) {
+
+        this.realm = realm;
+    }
+
+    public void saveReservation(final RealmReservation realmReservation){
+
+        realm.executeTransaction(new Realm.Transaction() {
+
+            @Override
+            public void execute(Realm realm) {
+
+                realm.copyToRealm(realmReservation);
+
+            }
+        });
+    }
+
+    /*
+    Returns list of all customers.
+     */
+
+    public List<RealmReservation> getreservations(){
+
+        List<RealmReservation> reservations = new ArrayList<>();
+
+        RealmResults<RealmReservation> realmTracksRealmResults = realm.where(RealmReservation.class).findAll();
+
+        for(RealmReservation realmReservation: realmTracksRealmResults){
+            reservations.add(realmReservation);
+        }
+
+        return reservations;
+
+    }
+
+
+}
